b51cc7a4b66016431ad6088d6fef9e389a88e691
Paul Richardson <developer@photographerafoot.com>
2021-04-05 16:17:36 +1000

    CHG: We only use one section

--- FlagViewer/TableViewController.swift
+++ FlagViewer/TableViewController.swift
@@ -23,11 +23,6 @@ class TableViewController: UITableViewController {
 
 	// MARK: - Table view data source
 
-	override func numberOfSections(in tableView: UITableView) -> Int {
-		// #warning Incomplete implementation, return the number of sections
-		return 0
-	}
-
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		// #warning Incomplete implementation, return the number of rows
 		return countries.count
