08af19f591d5987781e77f109f25438c12c23c17
Paul Richardson <developer@photographerafoot.com>
2021-04-05 16:24:09 +1000

    CHG: add country property

--- FlagViewer/FlagViewController.swift
+++ FlagViewer/FlagViewController.swift
@@ -9,21 +9,23 @@ import UIKit
 
 class FlagViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	var country: Country?
 
-        // Do any additional setup after loading the view.
-    }
-    
+	override func viewDidLoad() {
+		super.viewDidLoad()
 
-    /*
-    // MARK: - Navigation
+		// Do any additional setup after loading the view.
+	}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+
+	/*
+	// MARK: - Navigation
+
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+	// Get the new view controller using segue.destination.
+	// Pass the selected object to the new view controller.
+	}
+	*/
 
 }
