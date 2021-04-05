e1689ce2d9e89ddfbc18870ec17214752fa4cba9
Paul Richardson <developer@photographerafoot.com>
2021-04-06 08:02:29 +1000

    CHG: Clean up

--- FlagViewer/TableViewController.swift
+++ FlagViewer/TableViewController.swift
@@ -13,18 +13,11 @@ class TableViewController: UITableViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
-		// Uncomment the following line to preserve selection between presentations
-		// self.clearsSelectionOnViewWillAppear = false
-
-		// Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-		// self.navigationItem.rightBarButtonItem = self.editButtonItem
 	}
 
 	// MARK: - Table view data source
 
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		// #warning Incomplete implementation, return the number of rows
 		return countries.count
 	}
 
@@ -36,21 +29,13 @@ class TableViewController: UITableViewController {
 		return cell
 	}
 
+	// MARK: - Table view delegate
+	
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		if let vc = storyboard?.instantiateViewController(identifier: "FlagViewController") as? FlagViewController {
 			vc.country = countries[indexPath.row]
 			navigationController?.pushViewController(vc, animated: true)
 		}
 	}
-	
-	/*
-	// MARK: - Navigation
-
-	// In a storyboard-based application, you will often want to do a little preparation before navigation
-	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-	// Get the new view controller using segue.destination.
-	// Pass the selected object to the new view controller.
-	}
-	*/
 
 }
