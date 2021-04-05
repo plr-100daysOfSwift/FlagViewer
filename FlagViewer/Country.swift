9ad3966b239c5c86d4a74fe59ceded792a311f2e
Paul Richardson <developer@photographerafoot.com>
2021-04-05 16:01:14 +1000

    CHG: add Country enum

---
+++ FlagViewer/Country.swift
@@ -0,0 +1,12 @@
+//
+//  Country.swift
+//  FlagViewer
+//
+//  Created by Paul Richardson on 05/04/2021.
+//
+
+import Foundation
+
+enum Country: CaseIterable {
+	case estonia, france, germany, ireland, italy, monaco, nigeria, poland, russia, spain, uk, us
+}
